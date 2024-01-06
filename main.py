c149cbfd176029f25a47a9a6b947cb660f2b8592
fingerhair <tempoearlvoid@gmail.com>
2024-01-06 02:52:10 -0500

    Gets list of top hip hop songs from lastfm Need logic to verify each element is album not single

--- main.py
+++ main.py
@@ -1,60 +1,38 @@
 from dotenv import load_dotenv
 import os
-import base64
-from requests import post, get
-import json
+import requests
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy_random import get_random
 
 
 load_dotenv()
+spotify_client_id = os.getenv("SPOTIFY_CLIENT_ID")
+spotify_client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
+spotify_client = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
+    client_id=spotify_client_id,
+    client_secret=spotify_client_secret))
+lastfm_api_key = os.getenv("LASTFM_CLIENT_ID")
 
-client_id = os.getenv("CLIENT_ID")
-client_secret = os.getenv("CLIENT_SECRET")
-
-
-def get_token():
-    auth_string = client_id + ":" + client_secret
-    auth_bytes = auth_string.encode("utf-8")
-    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
-
-    url = "https://accounts.spotify.com/api/token"
-    headers = {
-        "Authorization": "Basic " + auth_base64,
-        "Content-Type": "application/x-www-form-urlencoded"
+def get_album_list_by_genre(genre):
+    url = "http://ws.audioscrobbler.com/2.0/"
+    parameters = {
+        "method": "tag.gettopalbums",
+        "tag": genre,
+        "api_key": lastfm_api_key,
+        "format": "json"
     }
-    data = {"grant_type": "client_credentials"}
-    result = post(url, headers=headers, data=data)
-    json_result = json.loads(result.content)
-    myToken = json_result["access_token"]
-    return myToken
 
-def get_auth_header(token):
-    return {"Authorization": "Bearer " + token}
+    response = requests.get(url, params=parameters)
+    jsondict = response.json()
+    return jsondict['albums']['album']
 
-def search_for_artist(token, artist_name):
-    url = "https://api.spotify.com/v1/search"
-    headers = get_auth_header(token)
-    query = f"?q={artist_name}&type=artist&limit=1"
 
-    query_url = url + query
-    result = get(query_url, headers=headers)
-    json_result = json.loads(result.content)["artists"]["items"]
-    if len(json_result) == 0:
-        print("No artist with this name exists...")
-        return None
+hiphopalbumjson = get_album_list_by_genre("hip-hop")
+hiphopalbumlist = []
+for album in hiphopalbumjson:
+    album_name = album['name']
+    hiphopalbumlist.append(album_name)
 
-    return json_result[0]
-
-def get_songs_by_artist(token, artist_id):
-    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
-    headers = get_auth_header(token)
-    result = get(url, headers=headers)
-    json_result = json.loads(result.content)["tracks"]
-    return json_result
-
-token = get_token()
-result = search_for_artist(token, "Earl Sweatshirt")
-artist_id = result["id"]
-songs = get_songs_by_artist(token, artist_id)
-
-for idx, song in enumerate(songs):
-    print(f"{idx + 1}. {song['name']}")
+for i in hiphopalbumlist:
+    print(i)
